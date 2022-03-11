1a533f997254dcb859476521acb6a8e35ff54221
adebayo935 <85680990+adebayo935@users.noreply.github.com>
2022-03-11 09:57:59 +0100

    Add files via upload

---
+++ lettings/tests.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from django.urls import reverse
+from .models import Letting,Address
+
+
+class LettingTestCase(TestCase):
+    def test_should_showIndex(self):
+        response = reverse('lettings_index')
+        assert response
+
+
+    def test_should_showLetting(self):
+        response = reverse('letting',args=[1])
+        assert response
