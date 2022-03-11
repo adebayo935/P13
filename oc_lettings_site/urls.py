cd1e9e916d08d3ce71b623b4266b762ee593ce3e
adebayo935 <85680990+adebayo935@users.noreply.github.com>
2022-03-11 10:00:36 +0100

    Add files via upload

---
+++ oc_lettings_site/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+
+from lettings import views as let
+from profiles import views as pro
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('lettings/', let.index, name='lettings_index'),
+    path('lettings/<int:letting_id>/', let.letting, name='letting'),
+    path('profiles/', pro.index, name='profiles_index'),
+    path('profiles/<str:username>/', pro.profile, name='profile'),
+    path('admin/', admin.site.urls),
+]
