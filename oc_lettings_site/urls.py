945330e42a474d0ef0b2a3a8073b9a7936e98ee3
adebayo935 <85680990+adebayo935@users.noreply.github.com>
2022-03-23 10:59:10 +0100

    Update urls.py

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -5,9 +5,11 @@ from lettings import views as let
 from profiles import views as pro
 from . import views
 
+
 def trigger_error(request):
     division_by_zero = 1 / 0
     print(division_by_zero)
+
     
 urlpatterns = [
     path('', views.index, name='index'),
