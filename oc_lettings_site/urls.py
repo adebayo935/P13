62eb8b38c760d4fef4a9bc3c26ac576d216f88a7
adebayo935 <85680990+adebayo935@users.noreply.github.com>
2022-03-23 11:16:49 +0100

    Update urls.py

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -6,11 +6,6 @@ from profiles import views as pro
 from . import views
 
 
-def trigger_error(request):
-    division_by_zero = 1 / 0
-    print(division_by_zero)
-
-
 urlpatterns = [
     path('', views.index, name='index'),
     path('lettings/', let.index, name='lettings_index'),
@@ -18,5 +13,5 @@ urlpatterns = [
     path('profiles/', pro.index, name='profiles_index'),
     path('profiles/<str:username>/', pro.profile, name='profile'),
     path('admin/', admin.site.urls),
-    path('sentry-debug/', trigger_error),
+    path('sentry-debug/', views.trigger_error),
 ]
