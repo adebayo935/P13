22a3769a55fc05dff1476e4e2d549282542ce3b3
adebayo935 <85680990+adebayo935@users.noreply.github.com>
2023-03-06 11:49:57 +0100

    Update models.py

--- lettings/models.py
+++ lettings/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinLengthValidator
 
-
+e
 class Address(models.Model):
     number = models.PositiveIntegerField(validators=[MaxValueValidator(9999)])
     street = models.CharField(max_length=64)
